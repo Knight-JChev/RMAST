614a8dc7c6054054759145579cc594f1a38981ae
Julien Chevreau <chevreau.julien21@gmail.com>
2025-05-21 11:14:47 +0200

    Fonctionne sur plusieurs marqueurs et niveaux de similarité

--- graphes_real_data.R
+++ graphes_real_data.R
@@ -1,57 +1,85 @@
 library(dplyr)
 library(ggplot2)
+library(stringr)
 
-resdir ="/home/knight/Bureau/Rouen_M1/Stage_LECA/Vert01/Results"
+resdir ="/home/knight/Bureau/Rouen_M1/Stage_LECA/KAST/Results/"
 
-sim_thr =c(0.75, 0.8, 0.85, 0.90, 0.95)
-thr = 0.95
+# Vecteur de seuils de similarités
+comptes = list.files(resdir, pattern = "0\\.[0-9]*\\.tsv$")
+kast_tables = list.files(resdir, pattern="max\\.tsv")
+
+# Initialise Data frames
 all_clusters = data.frame()
 all_kast = data.frame()
 
-for (thr in sim_thr) {
-  clusters = as.data.frame(t(read.table(
-    paste0(resdir,"/DB.Vert01.fasta_comptes_cluster_", thr ,".tsv"), 
+# Lecture des données ####
+# Build the table with cluster distribution
+for (file in comptes) {
+  clusters = as.data.frame(t(read.table(paste0(resdir,file), 
     sep="\t", header = F, colClasses=c("integer"))))
   
+  barcode = str_extract(file, "DB\\.(.+?)\\.", group = 1)
+  thr = str_extract(file, "(0\\.[0-9]+)\\.tsv", group = 1)
+    
   clusters = clusters %>%
-    mutate(similarity = thr)
+    mutate(similarity = thr, barcode = barcode) # Add columns for later grouping
   
   all_clusters = rbind(all_clusters, clusters)
-  
-  kast = read.table(
-    paste0(resdir,"/kast_table_", thr,"_similarity.tsv"), sep="\t",
+}
+
+# Build the table with kast results
+for (file in kast_tables){
+  kast = read.table(paste0(resdir, file), sep="\t",
     header = T, row.names=1)
+   
+  barcode = str_extract(file, "table_(.+?)\\.", group = 1)
+  thr = str_extract(file, "_(0\\.[0-9]+)_", group = 1)
+  
   kast = kast %>%
-    mutate(similarity = thr)
+    select(!c(taxo,phylo)) %>% # Remove trees
+    mutate(similarity = thr, barcode = barcode) # Add columns for later grouping
   
   all_kast = rbind(all_kast, kast)
-  
 }
+
+# Conversion to factors
 all_clusters$similarity = as.factor(all_clusters$similarity)
+all_clusters$barcode = as.factor(all_clusters$barcode)
+
 all_kast$similarity = as.factor(all_kast$similarity)
+all_kast$barcode = as.factor(all_kast$barcode)
 
-
-cluster_test = as.data.frame(all_clusters[10<all_clusters[,1],])
-
-all_clusters %>% dplyr::count(V1, similarity) %>%
-  ggplot() + geom_point(aes(x = log(V1), y = log(n))) +
-  geom_vline(aes(xintercept = log(11)), colour = "red") +
-  geom_vline(aes(xintercept = log(150)), colour = "red")
-
-all_clusters %>% dplyr::count(V1, similarity) %>%
-  ggplot() + geom_point(aes(x = V1, y = n, colour = similarity)) +
-  geom_vline(aes(xintercept = 11), colour = "lightgreen") +
-  geom_vline(aes(xintercept = 150), colour = "lightgreen")+
+# Plots ####
+# Dot plot
+all_clusters %>% dplyr::count(V1, barcode, similarity) %>%
+  ggplot() +
+  facet_grid(barcode~similarity) +
+  geom_point(aes(x = V1, y = n, colour = similarity)) +
+  geom_vline(aes(xintercept = 20), colour = "red") +
+  geom_vline(aes(xintercept = 150), colour = "red") +
   labs(x="Cluster size",
        y = "Nombre d'observation") +
   scale_x_log10()+
   scale_y_log10()
-  
-all_clusters %>% dplyr::count(V1, similarity) %>%
-  ggplot() + geom_density(aes(x = V1, colour = similarity))+
+
+
+# Density plot à réfléchir  
+all_clusters %>% dplyr::count(V1,barcode, similarity) %>%
+  ggplot() + 
+  facet_grid(barcode~.) +
+  geom_density(aes(x = V1, colour = similarity))+
   labs(x = "Cluster size") +
   scale_x_log10()
 
+# Manipulation de tableaux ####
+# Nombre de clusters de taille < 20
+all_clusters %>%
+  group_by(similarity, barcode) %>%
+  count(V1, barcode, similarity, name = "count") %>%
+  filter(V1<20) %>%
+  tally(count)
 
-class(as.data.frame(all_clusters))
-
+# Nombre de clusters analysés
+all_kast %>%
+  group_by(similarity, barcode) %>%
+  count(similarity, barcode)
